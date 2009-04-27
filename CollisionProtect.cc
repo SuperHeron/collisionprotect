80b69119d4d917aa31891a02667aef5b6a8fe570
Pierre Lejeune <superheron@gmail.com>
2009-04-27 20:34:53 +0200

    Forgot to comment out somme debug text

--- CollisionProtect.cc
+++ CollisionProtect.cc
@@ -320,7 +320,7 @@ paludis::HookResult paludis_hook_run(const paludis::Environment* env, const palu
 /*
  * Find installed package being replaced
  */
-		std::cout << "Getting list of files of possibly old package version..." << std::endl;
+//		std::cout << "Getting list of files of possibly old package version..." << std::endl;
 //		std::tr1::shared_ptr<const paludis::PackageIDSequence> oldpkg((*env)[paludis::selection::BestVersionInEachSlot(paludis::generator::Package(packageName) | paludis::filter::SupportsAction<paludis::InstalledAction>())]);
 		std::tr1::shared_ptr<const paludis::PackageIDSequence> oldpkg((*env)[paludis::selection::BestVersionInEachSlot(paludis::generator::Intersection(
 																													   paludis::generator::Package(packageName),
